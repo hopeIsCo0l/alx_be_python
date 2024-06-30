82b6e71a4857026638e8e2081b8d39fa219a22ea
hopeIsCo0l <163678847+hopeIsCo0l@users.noreply.github.com>
2024-06-30 22:56:24 +0300

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -10,10 +10,10 @@ def main():
     elif weather == "rainy":
         print("Don't forget your umbrella and a raincoat.")
     elif weather == "cold":
-        #print("Make sure to wear a warm coat and a scarf.")
-    else:
-        print("Sorry, I don't have recommendations for this weather.")
-        print("Sorry, I don't have recommendations for this weather.")
+        print("Make sure to wear a warm coat and a scarf.")
+    # else:
+    #     print("Sorry, I don't have recommendations for this weather.")
+    #     print("Sorry, I don't have recommendations for this weather.")
 
 if __name__ == "__main__":
     main()
