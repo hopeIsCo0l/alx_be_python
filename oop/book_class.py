f0c7e8d45c55db8884bef37e8c0004942e269d28
Hope <hope@hope.aait>
2024-08-28 17:34:51 +0300

    Update book_class.py to remove creation message

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,13 +5,16 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Creating {self}")
+        # Removed the creation message from __init__
 
     def __del__(self):
+        # This will print the deletion message as specified
         print(f"Deleting {self.title}")
 
     def __str__(self):
+        # This returns the string representation of the book
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
+        # This returns the official representation of the book
         return f"Book('{self.title}', '{self.author}', {self.year})"
