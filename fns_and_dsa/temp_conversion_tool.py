2599599181e2986f8424d46e74ca310b03c69f08
Amanuel <amanuel.ayalew@aait.edu.et>
2024-09-20 14:18:18 +0300

    fix2

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,7 @@
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 FREEZING_POINT_DIFFERENCE = 32
 
 # Function to convert Fahrenheit to Celsius
