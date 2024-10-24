90e19f13e1180a9b4363d07db96e7bf1095ac254
hopeIsCo0l <abdellahteshome@outlook.com>
2024-10-24 13:50:11 +0300

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -9,11 +9,9 @@ def main():
         print("Wear a t-shirt and sunglasses.")
     elif weather == "rainy":
         print("Don't forget your umbrella and a raincoat.")
-    # elif weather == "cold":
-    #     print("Make sure to wear a warm coat and a scarf.")
+    elif weather == "cold":
+        print("Make sure to wear a warm coat and a scarf.")
     else:
-        # Handle unexpected input
-        print("Sorry, I don't have recommendations for this weather.")
-
+        # print("Sorry, I don't have recommendations for this weather.")
 if __name__ == "__main__":
     main()
